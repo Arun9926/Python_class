f7cbd8e3534a829bc6935cf677b79d2cf060f039
arun patel <arunpatel12041998@gmail.com>
2025-01-21 17:52:59 +0530

    pp

--- slice.py
+++ slice.py
@@ -1,7 +1,46 @@
-str="I love python"
+# str="I love python"
 # print(str[::])
 # print(str[::-1])
 # print(str[-3:4])
 # print(str[:5:-2])
 # print(str[-8:-1:2])
-print(str[-4:-8:-1])
+# print(str[-4:-8:-1])
+
+
+list=[1,2,3,4,5,6,7,8,9]
+# print(list[1:4])
+# print(list[:])
+# print(list[::])
+
+# b=list[2:]
+# print(b)
+
+# c=list[:3]
+# print(c)
+
+
+# d=list[1:4]
+# print(d)
+
+# e=list[::2]
+# print(e)
+
+
+# f=list[1:8:3]
+# print(f)
+
+
+# f=list[-2:]
+# print(f)
+
+# f=list[:-3]
+# print(f)
+
+# f=list[-4:-1]
+# print(f)
+
+# f=list[-8:-1:2]
+# print(f)
+
+# f=list[::-1]
+# print(f)

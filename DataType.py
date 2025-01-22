a405a8a7f40124461e4316f86847d05d8f6acdfe
arun patel <arunpatel12041998@gmail.com>
2025-01-22 14:28:12 +0530

    py

---
+++ DataType.py
@@ -0,0 +1,64 @@
+#  numeric
+    #  Integer
+# x=10
+# y=20
+# print(type(x))
+# print(type(y))
+
+# a=x+y
+# print(type(a))
+# b=x*y
+# print(type(b))
+# c=x-y
+# print(type(c))
+
+# z=x/y
+# print(type(z))#float
+#======================================================================
+
+#float
+# x=10.5
+# y=20.5
+# print(type(x))
+# print(type(y))
+
+# a=x+y
+# print(type(a))
+# b=x*y
+# print(type(b))
+# c=x-y
+# print(type(c))
+
+# z=x/y
+# print(type(z))
+#=====================================================================
+
+#COMPLEX
+# x=10.5+3j
+# y=20.5+3j
+# print(type(x))
+# print(type(y))
+
+# a=x+y
+# print(type(a))
+# b=x*y
+# print(type(b))
+# c=x-y
+# print(type(c))
+
+# z=x/y
+# print(type(z))
+#====================================================================
+
+#String
+s1='Pyhton'
+# print(type(s1))
+
+# x=input("Enter your name : ")
+# print(x)
+# print(type(x))
+
+
+print(max(s1))
+print(min(s1))
+print(len(s1))
--- slice.py
+++ slice.py
@@ -1,10 +1,10 @@
-# str="I love python"
-# print(str[::])
-# print(str[::-1])
-# print(str[-3:4])
-# print(str[:5:-2])
-# print(str[-8:-1:2])
-# print(str[-4:-8:-1])
+# str1="I love python"
+# print(str1[::])
+# print(str1[::-1])
+# print(str1[-3:4])
+# print(str1[:5:-2])
+# print(str1[-8:-1:2])
+# print(str1[-4:-8:-1])
 
 
 list=[1,2,3,4,5,6,7,8,9]

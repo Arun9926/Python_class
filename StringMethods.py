05230d856ae3092c68810e87d94194356cb3ef6f
arun patel <arunpatel12041998@gmail.com>
2025-01-23 14:24:52 +0530

    string methods

---
+++ StringMethods.py
@@ -0,0 +1,25 @@
+str1='I love python'
+# print(str1.lower())
+# print(str1.upper())
+# print(str1.title())
+# print(str1.capitalize())
+# print(str1.swapcase())
+# # print(str1.index('z')) #given error
+# print(str1.find('p'))
+# print(str1.find('z'))     
+
+
+#split
+# print(str1.split()) #given output is list form
+# print(str1.split('o'))
+# print(str1.split('o',1))
+
+#join
+# l1=['Arun','Kumar','Patel']
+# # print(','.join(l1))#output datatype is string
+# l=','.join(l1)
+# print(type(l))
+
+
+#count
+print(str1.count('o'))

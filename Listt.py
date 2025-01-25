546c919c8cc15a061ca7c81f1af6759acf523b12
arun patel <arunpatel12041998@gmail.com>
2025-01-25 14:23:40 +0530

    tuple

--- Listt.py
+++ Listt.py
@@ -11,7 +11,7 @@
 # max(),sum(),min() same datatype par nikalta hai
 
 
-l1=[10,10.5,"Arun",2,4,8]
+# l1=[10,10.5,"Arun",2,4,8]
 #============================================
 #python functions
 #============================================
@@ -22,14 +22,58 @@ l1=[10,10.5,"Arun",2,4,8]
 #=============================================
 #List methods
 #=============================================
-print(l1.append())#add one object at last position
-print(l1.sort())#arrenge assending form
-print(l1.extend())#add multiple object in last position
-print(l1.remove())#remove one object from required position 
-print(l1.reverse())#to arrenge in reverse order
-print(l1.clear())# remove all object from list
-print(l1.pop())#remove one object from last position 
-print(l1.insert())#add one object from required position
-print(l1.copy())# create another object with same content
-print(l1.count())# frequeny
-print(l1.index())# object position
+# print(l1.append())#add one object at last position
+# print(l1.sort())#arrenge assending form
+# print(l1.extend())#add multiple object in last position
+# print(l1.remove())#remove one object from required position 
+# print(l1.reverse())#to arrenge in reverse order
+# print(l1.clear())# remove all object from list
+# print(l1.pop())#remove one object from last position 
+# print(l1.insert())#add one object from required position
+# print(l1.copy())# create another object with same content
+# print(l1.count())# frequeny
+# print(l1.index())# object position
+
+# l1=[10,20,20.5,2,8,6,1]
+# l1.sort()
+# print(l1)
+
+# l1=[10,20,20.5,2,8,6,1]
+# l1.reverse()
+# print(l1)
+
+# l1=[10,20,20.5,2,8,6,1]
+# l1.sort(reverse=True)
+# print(l1)
+
+
+# l1=[10,20,20.5,2,8,6,1]
+# l1.pop()
+# print(l1)
+
+
+# l1=[10,20,20.5,2,8,6,1]
+# l1.remove(20.5)
+# print(l1)
+
+
+
+# l1=[10,20,20.5,2,8,6,1]
+# l1.clear()
+# print(l1)
+
+
+# l1=[10,20,20.5,2,8,6,1]
+# l2=l1.copy()
+# print(l1)
+# print(l2)
+# print(id(l1))
+# print(id(l2))
+
+
+# l1=[10,20,20.5,2,8,6,10]
+# print(l1.count(10))
+
+
+l1=[10,20,20.5,2,8,6,10]
+print(l1.index(1,2))
---
+++ tuplee.py
@@ -0,0 +1,25 @@
+# Collection of elements in a single objects
+# ordered collection
+# indexing supported
+# slicing supported
+# immutable in nature
+# represent in () with comma ',' seprated element
+# tuple is faster as compare to the list(Due to immutable nature as well as less requird space for creation)
+
+
+t1=(10,20,30,40,50,60)
+# print(t1)
+# print(type(t1))
+
+# print(max(t1))
+# print(min(t1))
+# print(sum(t1))
+# print(len(t1))
+# print(id(t1))
+# print(type(t1))
+
+#===============================================
+# methods in tuble
+#===============================================
+print(t1.count(10))
+print(t1.index(10))

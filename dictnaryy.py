518301eb2f79692d61c117b438d6959dd6a66652
arun patel <arunpatel12041998@gmail.com>
2025-02-02 13:51:05 +0530

    dictionary Question

--- dictnaryy.py
+++ dictnaryy.py
@@ -51,6 +51,29 @@ d1={"name":"Arun","age":26,"quals":"BE"}
 # d1.setdefault('from','bhopal')
 # print(d1)
 
-d2={}
-d1.update(d2)
-print(d1)
+# d2={}
+# d1.update(d2)
+# print(d1)
+
+# dc={0: 10, 1: 20}
+# dc.update({2:30})
+# print(dc)
+
+# given dict add into another dictionary
+dic1={1:10, 2:20}
+dic2={3:30, 4:40}
+dic3={5:50,6:60}
+dic4={}
+for d in (dic1,dic2,dic3):
+    dic4.update(d)
+
+print(dic4)
+
+# given key present or not
+def key_is_present(x):
+    if x in dic4:
+        print("key is present")
+    else:
+        print("key is not present")
+
+key_is_present(7)

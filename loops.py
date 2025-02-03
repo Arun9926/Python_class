95d4d007efe3b1942bcec556fc7932e2f0543fcd
arun patel <arunpatel12041998@gmail.com>
2025-02-03 14:30:44 +0530

    while loop

---
+++ loops.py
@@ -0,0 +1,15 @@
+# x=int(input("Enter number : "))
+# i=1
+# while i<=x:
+#     print(i,end=',')
+#     i+=1
+    
+# print(i,x)
+
+n=int(input("Enter number : "))
+i=1
+while i<=n:
+    x=2*i
+    # print(f'2 x {i} = {x}')
+    print(x)
+    i+=1

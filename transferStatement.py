630242f592a8b31c50936d4e35a07802aef462f2
arun patel <arunpatel12041998@gmail.com>
2025-02-06 14:29:50 +0530

    ts

---
+++ transferStatement.py
@@ -0,0 +1,25 @@
+#break -> exit from loop
+#continue -> skip current iteration
+#pass -> skip current block
+
+# i=1
+# while i<=10:
+#     if i==6:
+#         break
+#     print(i)
+#     i+=1
+# print('hello')    
+
+
+# i=1
+# while i<=10:
+#     if i==6:
+#         i+=1
+#         continue
+#     print(i)
+#     i=i+1
+# print("hello")
+
+x=eval(input('enter value : '))
+print(x)
+print(type(x))

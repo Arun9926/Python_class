830f8df1eefb2edd37e65bbeea047a9d5141c8c1
arun patel <arunpatel12041998@gmail.com>
2025-02-14 14:26:49 +0530

    scop

--- variable_key-word_arguments.py
+++ variable_key-word_arguments.py
@@ -12,8 +12,24 @@
 #     print(type(n))
 # show_my_detail(name="Arun",age=26,quali="B.E")    
 
-def show_my_detail(**n):
-    print(n)
-    print(type(n))
-x=eval(input("Enter any dict : "))        
-show_my_detail(**x)    
+# def show_my_detail(**n):
+#     print(n)
+#     print(type(n))
+# x=eval(input("Enter any dict : "))        
+# show_my_detail(**x)    
+
+# x=10
+# def add():
+#     global x
+#     print(x)
+#     x=20
+#     print(x)
+# add()
+# print(x)
+x=10
+def add():
+    x=20
+    print(globals()['x'])
+    print(x)
+add()
+print(x)

d03ab4406dc192f82708771949fa46f47cffb6ec
arun patel <arunpatel12041998@gmail.com>
2025-02-19 14:24:24 +0530

    filter reduce

--- functions.py
+++ functions.py
@@ -94,10 +94,87 @@
 # print(list(res))
 
 
-def add(x,y,z):
-    return x+y+z
-l1 = eval(input("Enter any numeric collection : "))
-l2 = eval(input("Enter any numeric collection : "))
-l3 = eval(input("Enter any numeric collection : "))
-res=map(add,l1,l2,l3)
-print(list(res))
+# def add(x,y,z):
+#     return x+y+z
+# l1 = eval(input("Enter any numeric collection : "))
+# l2 = eval(input("Enter any numeric collection : "))
+# l3 = eval(input("Enter any numeric collection : "))
+# res=map(add,l1,l2,l3)
+# print(list(res))
+
+
+#====================================================
+# Filter
+#====================================================
+# l1=[-1,2,3,-4,5,6,-7,8]
+# def fun(x):
+#     if x%2==0:
+#         return x
+# res=filter(fun,l1)
+# print(list(res))    
+
+
+# def fun(x):
+#     if x%2!=0:
+#         return x
+# res=filter(fun,l1)
+# print(list(res))  
+
+
+# def fun(x):
+#     if x>=0:
+#         return x
+# res=filter(fun,l1)
+# print(list(res))  
+
+
+# def fun(x):
+#     if x<0:
+#         return x
+# res=filter(fun,l1)
+# print(list(res))        
+
+
+# def fun(x):
+#     if x%2==0:
+#         return 'even'
+#     else:
+#         return 'odd'
+# res=map(fun,l1)
+# print(list(res)) 
+
+
+# s=['arun','aman','raj','jai','rb']
+# def threeWord(x):
+#     if len(x)<=3:
+#         return x
+# res=filter(threeWord,s)     
+# print(list(res))   
+
+
+#=================================================
+# Reduce
+#=================================================
+import functools
+l1=[2,15,6,18,10]
+def my_max(x,y):
+    if x>y:
+        return x
+    else:
+        return y
+res=functools.reduce(my_max,l1)
+print("max=",res)
+
+def my_min(x,y):
+    if x<y:
+        return x
+    else:
+        return y
+res=functools.reduce(my_min,l1)
+print("min=",res)
+
+
+def my_sum(x,y):
+    return x+y
+res=functools.reduce(my_sum,l1)
+print("sum=",res)

7b4f3f31795dc64761015218510bf972c493904c
arun patel <arunpatel12041998@gmail.com>
2025-03-06 14:25:27 +0530

    polymorphism

--- inheritance.py
+++ inheritance.py
@@ -43,16 +43,67 @@
 
 #==================================================================
 #3. Multi lavle inheritance
-class Grand_parent:
+# class Grand_parent:
+#     def home(self):
+#         print('Grand parent home')
+# class Parent(Grand_parent):
+#     def car(self):
+#         print('parent car')
+# class Child(Parent):
+#     def car2(self):
+#         print('child car2')
+# obj=Child()
+# obj.home()
+# obj.car()
+# obj.car2()
+
+#=================================================================
+#4. Herarichical inheritance
+# class Parent:
+#     def home(self):
+#         print("parent class")
+# class Child1(Parent):
+#     def home1(self):
+#         print("child1 class")
+# class Child2(Parent):
+#     def home2(self):
+#         print("child2 class")
+# obj = Child1()
+# obj.home()
+# obj1 = Child2()
+# obj1.home()
+
+
+class Parent:
     def home(self):
-        print('Grand parent home')
-class Parent(Grand_parent):
-    def car(self):
-        print('parent car')
-class Child(Parent):
-    def car2(self):
-        print('child car2')
-obj=Child()
+        print("parent class")
+class Child1(Parent):
+    def home(self):
+        # print("child1 class")
+        super().home()
+class Child2(Parent):
+    def home(self):
+        print("child2 class")
+obj = Child1()
 obj.home()
-obj.car()
-obj.car2()
+obj1 = Child2()
+obj1.home()
+
+
+#=====================================================================
+# 5.Hybrid inheritance
+# class Parent:
+#     def home(self):
+#         print("parent class")
+# class Child1(Parent):
+#     def home(self):
+#         print("child1 class")
+# class Child2(Parent):
+#     def home(self):
+#         print("child2 class")
+# class Child3(Child1,Child2):
+#     def new(self):
+#         print("child3 class")
+# obj = Child3()
+# obj.home()
+# print(Child3.__mro__)
